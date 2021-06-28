52d85bc9d4bed93e1ce8b5072eae07a0755ccd23
nilson.silva@sankhya.com.br <Luci@ne123>
2021-06-28 19:02:05 -0300

    criando projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {AppBody, Button, Header, HeaderText} from "./styles";
+
+const App = () => {
+    return (
+        <AppBody>
+            <Header>
+                <HeaderText>Aplicação de teste</HeaderText>
+                <Button>Pesquisar</Button>
+            </Header>
+
+        </AppBody>
+    );
+}
+
+export default App;
+
---
+++ src/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderText = styled.span`
+  font-size: 24px;
+  font-weight: 500;
+  color: #212121;
+`;
+
+export const AppBody = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  border-radius: 5px;
+  color: #3AA335;
+  font-weight: bold;
+  border: 1px solid #7914ec;
+  background: white;
+  cursor: pointer;
+  transition: all ease-in-out 0.4s;
+
+  &:hover {
+    background: #d9d8d8;
+  }
+`;
